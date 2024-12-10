d2a43bcea231780beadecc2161351d8af050bc02
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-10 10:06:05 +0000

    Committing Task 2.

---
+++ Task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var currentYear = DateTime.Now.Year;
+        var birthYear = 2006;
+        var age = currentYear - birthYear;
+        Console.WriteLine("Hello, Sam!");
+        Console.Write("Your age is ");
+        Console.Write(age);
+        Console.WriteLine(".");
+    }
+}
