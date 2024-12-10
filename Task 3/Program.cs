c1690886aaed9d839f468119473d00803b6e15b8
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2024-12-10 10:09:51 +0000

    Completed and versioned Task 3.

---
+++ Task 3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("A duck's quack doesnt echo.");
+        Console.WriteLine(false);
+        Console.WriteLine("");
+        Console.WriteLine("You can use camel antibodies to help fight cancer. These are called \"nanobodies\" and are much smaller than regular antibodies.");
+        Console.WriteLine(true);
+    }
+}
